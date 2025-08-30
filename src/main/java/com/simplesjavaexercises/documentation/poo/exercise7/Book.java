9819676e3f7f48a8f69351695285bf54efbbbad9
Angélica Martins <marttinsangelica@gmail.com>
2025-08-30 09:23:29 -0300

    add new exercise

---
+++ src/main/java/com/simplesjavaexercises/documentation/poo/exercise7/Book.java
@@ -0,0 +1,32 @@
+package com.simplesjavaexercises.documentation.poo.exercise7;
+
+public class Book {
+
+  private String title;
+  private int pages;
+  private String[] authors;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public int getPages() {
+    return pages;
+  }
+
+  public void setPages(int pages) {
+    this.pages = pages;
+  }
+
+  public String[] getAuthors() {
+    return authors;
+  }
+
+  public void setAuthors(String[] authors) {
+    this.authors = authors;
+  }
+}
---
+++ src/main/java/com/simplesjavaexercises/documentation/poo/exercise7/Main.java
@@ -0,0 +1,51 @@
+package com.simplesjavaexercises.documentation.poo.exercise7;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int value = 10;
+    Book book = new Book();
+
+    System.out.println("Value before modifyPrimitive = " + value);
+    modifyPrimitive(value);
+    System.out.println("Value after modifyPrimitive = " + value);
+
+    System.out.println("---------------------------------");
+
+    System.out.println("Book before modifyReference = " + book.getTitle() + " " + book.getPages());
+    modifyReference(book);
+    System.out.println("Value after modifyReference = " + book.getTitle() + " " + book.getPages());
+
+    System.out.println("---------------------------------");
+
+    showAllAuthors(book.getAuthors());
+
+    System.out.println("---------------------------------");
+
+    String[] authors = {"Immanuel Kant", "René Descartes"};
+    showAllAuthors(authors);
+
+    System.out.println("---------------------------------");
+
+    showAllAuthors("Sócrates", "Platão", "Aristóteles");
+  }
+
+  public static void modifyPrimitive(int p) {
+    p = 13;
+  }
+
+  public static void modifyReference(Book b) {
+    b.setTitle("Any title");
+    b.setPages(1234);
+
+    String[] authors = {"Clarice Lispector", "Cecília Meireles"};
+    b.setAuthors(authors);
+  }
+
+  public static void showAllAuthors(String... authors) {
+    for (var author : authors) {
+      System.out.println(author);
+    }
+  }
+
+}
