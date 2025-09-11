12da02e3ee48ca8c59671ac58e0d45333537ad08
Angélica Martins <marttinsangelica@gmail.com>
2025-09-11 19:10:55 -0300

    add new exercises

--- src/main/java/com/simplesjavaexercises/documentation/poo/exercise10/Dog.java
+++ src/main/java/com/simplesjavaexercises/documentation/poo/exercise10/Dog.java
@@ -1,4 +1,23 @@
 package com.simplesjavaexercises.documentation.poo.exercise10;
 
 public class Dog {
+
+    private String name;
+    private int age;
+    private static int numberOfDogs;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+        numberOfDogs += 1;
+    }
+
+    public void bark() {
+        System.out.println(this.name);
+    }
+
+    public static int getNumberOfDogs() {
+        return numberOfDogs;
+    }
+
 }
--- src/main/java/com/simplesjavaexercises/documentation/poo/exercise10/Exercise10.java
+++ src/main/java/com/simplesjavaexercises/documentation/poo/exercise10/Exercise10.java
@@ -1,4 +1,21 @@
 package com.simplesjavaexercises.documentation.poo.exercise10;
 
 public class Exercise10 {
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Beethoven", 13);
+        Dog dog2 = new Dog("Mozart", 14);
+        Dog dog3 = new Dog("Vivaldi", 15);
+        Dog dog4 = new Dog("Bach", 16);
+        Dog dog5 = new Dog("Chopin", 17);
+
+        dog1.bark();
+        dog2.bark();
+        dog3.bark();
+        dog4.bark();
+        dog5.bark();
+
+        System.out.println(Dog.getNumberOfDogs());
+    }
+
 }
