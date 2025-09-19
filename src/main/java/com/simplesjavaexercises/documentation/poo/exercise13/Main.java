3feb49701c51f59e73728327d050893832329dab
Angélica Martins <marttinsangelica@gmail.com>
2025-09-19 08:55:00 -0300

    add new exercises

--- src/main/java/com/simplesjavaexercises/documentation/poo/exercise13/Main.java
+++ src/main/java/com/simplesjavaexercises/documentation/poo/exercise13/Main.java
@@ -3,8 +3,13 @@ package com.simplesjavaexercises.documentation.poo.exercise13;
 public class Main {
 
   public static void main(String[] args) {
-    var record = new RecordExample(1, 2);
+    var record = new RecordExample(3, 1);
     System.out.println(record);
+    System.out.println("a " + record.a());
+    System.out.println("b " + record.b());
+    record.printMessage("Message from an instance record");
+
+    RecordExample.printMessageStatic("Message from record");
   }
 
 }
--- src/main/java/com/simplesjavaexercises/documentation/poo/exercise13/RecordExample.java
+++ src/main/java/com/simplesjavaexercises/documentation/poo/exercise13/RecordExample.java
@@ -6,12 +6,26 @@ public record RecordExample(int a, int b) {
     System.out.println("Static initializer within a record");
   }
 
-  private static final int anyValue = 1;
+  private static final int anyValue = 13;
   private static int otherValue;
+
+  public RecordExample {
+    if (a > 2) a = 2;
+
+    if (b < 2) b = anyValue;
+  }
 
   @Override
   public String toString() {
     return "Override toString with static value: " + anyValue + " Other value: " + otherValue;
+  }
+
+  public void printMessage(String message) {
+    System.out.println(message);
+  }
+
+  public static void printMessageStatic(String message) {
+    System.out.println(message);
   }
 
 }
